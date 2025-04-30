cf86630c89188b5f1e203acdafb1d26a05c8e692
Danil0-1 <danilomuskus1@gmail.com>
2025-04-30 07:39:32 -0500

    feat: :lipstick: Se añadieron mas vehiculos a la pagina

--- src/components/vehicles.js
+++ src/components/vehicles.js
@@ -1,109 +1,240 @@
 const vehiculos = [
-    {
-      equipo: "Red Bull Racing",
-      modelo: "RB20",
-      motor: "Honda",
-      velocidad_maxima_kmh: 360,
-      aceleracion_0_100: 2.5,
-      pilotos: [1, 2],
-      rendimiento: {
-        conduccion_normal: {
-          velocidad_promedio_kmh: 320,
-          consumo_combustible: { seco: 1.9, lluvioso: 2.1, extremo: 2.4 },
-          desgaste_neumaticos: { seco: 1.5, lluvioso: 0.8, extremo: 2.5 }
-        },
-        conduccion_agresiva: {
-          velocidad_promedio_kmh: 340,
-          consumo_combustible: { seco: 2.4, lluvioso: 2.6, extremo: 3.0 },
-          desgaste_neumaticos: { seco: 2.2, lluvioso: 1.2, extremo: 3.5 }
-        },
-        ahorro_combustible: {
-          velocidad_promedio_kmh: 300,
-          consumo_combustible: { seco: 1.6, lluvioso: 1.8, extremo: 2.1 },
-          desgaste_neumaticos: { seco: 1.0, lluvioso: 0.5, extremo: 1.8 }
-        }
+  {
+    equipo: "Red Bull Racing",
+    modelo: "RB20",
+    motor: "Honda",
+    velocidad_maxima_kmh: 360,
+    aceleracion_0_100: 2.5,
+    pilotos: [1, 2],
+    rendimiento: {
+      conduccion_normal: {
+        velocidad_promedio_kmh: 320,
+        consumo_combustible: { seco: 1.9, lluvioso: 2.1, extremo: 2.4 },
+        desgaste_neumaticos: { seco: 1.5, lluvioso: 0.8, extremo: 2.5 }
       },
-      imagen: "https://cdn-7.motorsport.com/images/amp/0a98yMX0/s6/red-bull-racing-rb20.jpg"
+      conduccion_agresiva: {
+        velocidad_promedio_kmh: 340,
+        consumo_combustible: { seco: 2.4, lluvioso: 2.6, extremo: 3.0 },
+        desgaste_neumaticos: { seco: 2.2, lluvioso: 1.2, extremo: 3.5 }
+      },
+      ahorro_combustible: {
+        velocidad_promedio_kmh: 300,
+        consumo_combustible: { seco: 1.6, lluvioso: 1.8, extremo: 2.1 },
+        desgaste_neumaticos: { seco: 1.0, lluvioso: 0.5, extremo: 1.8 }
+      }
     },
-    {
-      equipo: "Mercedes-AMG Petronas",
-      modelo: "W15",
-      motor: "Mercedes",
-      velocidad_maxima_kmh: 355,
-      aceleracion_0_100: 2.6,
-      pilotos: [3, 4],
-      rendimiento: {
-        conduccion_normal: {
-          velocidad_promedio_kmh: 315,
-          consumo_combustible: { seco: 2.0, lluvioso: 2.2, extremo: 2.5 },
-          desgaste_neumaticos: { seco: 1.6, lluvioso: 0.9, extremo: 2.6 }
-        },
-        conduccion_agresiva: {
-          velocidad_promedio_kmh: 335,
-          consumo_combustible: { seco: 2.6, lluvioso: 2.8, extremo: 3.2 },
-          desgaste_neumaticos: { seco: 2.3, lluvioso: 1.4, extremo: 3.8 }
-        },
-        ahorro_combustible: {
-          velocidad_promedio_kmh: 295,
-          consumo_combustible: { seco: 1.7, lluvioso: 1.9, extremo: 2.2 },
-          desgaste_neumaticos: { seco: 1.1, lluvioso: 0.6, extremo: 1.9 }
-        }
+    imagen: "https://cdn-7.motorsport.com/images/amp/0a98yMX0/s6/red-bull-racing-rb20.jpg"
+  },
+  {
+    equipo: "Mercedes-AMG Petronas",
+    modelo: "W15",
+    motor: "Mercedes",
+    velocidad_maxima_kmh: 355,
+    aceleracion_0_100: 2.6,
+    pilotos: [3, 4],
+    rendimiento: {
+      conduccion_normal: {
+        velocidad_promedio_kmh: 315,
+        consumo_combustible: { seco: 2.0, lluvioso: 2.2, extremo: 2.5 },
+        desgaste_neumaticos: { seco: 1.6, lluvioso: 0.9, extremo: 2.6 }
       },
-      imagen: "https://www.raceland.eu/$WS/raceland/websale8_shop-raceland/produkte/medien/bilder/normal/20-17977.jpg"
+      conduccion_agresiva: {
+        velocidad_promedio_kmh: 335,
+        consumo_combustible: { seco: 2.6, lluvioso: 2.8, extremo: 3.2 },
+        desgaste_neumaticos: { seco: 2.3, lluvioso: 1.4, extremo: 3.8 }
+      },
+      ahorro_combustible: {
+        velocidad_promedio_kmh: 295,
+        consumo_combustible: { seco: 1.7, lluvioso: 1.9, extremo: 2.2 },
+        desgaste_neumaticos: { seco: 1.1, lluvioso: 0.6, extremo: 1.9 }
+      }
     },
-    {
-      equipo: "Ferrari",
-      modelo: "SF23",
-      motor: "Ferrari",
-      velocidad_maxima_kmh: 350,
-      aceleracion_0_100: 2.7,
-      pilotos: [5, 6],
-      rendimiento: {
-        conduccion_normal: {
-          velocidad_promedio_kmh: 310,
-          consumo_combustible: { seco: 2.1, lluvioso: 2.3, extremo: 2.7 },
-          desgaste_neumaticos: { seco: 1.7, lluvioso: 1.0, extremo: 2.8 }
-        },
-        conduccion_agresiva: {
-          velocidad_promedio_kmh: 330,
-          consumo_combustible: { seco: 2.7, lluvioso: 3.0, extremo: 3.5 },
-          desgaste_neumaticos: { seco: 2.5, lluvioso: 1.6, extremo: 4.0 }
-        },
-        ahorro_combustible: {
-          velocidad_promedio_kmh: 290,
-          consumo_combustible: { seco: 1.8, lluvioso: 2.0, extremo: 2.4 },
-          desgaste_neumaticos: { seco: 1.2, lluvioso: 0.7, extremo: 2.0 }
-        }
+    imagen: "https://p.turbosquid.com/ts-thumb/D5/Oaj3t2/DB/z0/jpg/1720609521/600x600/fit_q87/47e7201035cdbe117dd00fd6ef9db358c529e9f9/z0.jpg"
+  },
+  {
+    equipo: "Ferrari",
+    modelo: "SF23",
+    motor: "Ferrari",
+    velocidad_maxima_kmh: 350,
+    aceleracion_0_100: 2.7,
+    pilotos: [5, 6],
+    rendimiento: {
+      conduccion_normal: {
+        velocidad_promedio_kmh: 310,
+        consumo_combustible: { seco: 2.1, lluvioso: 2.3, extremo: 2.7 },
+        desgaste_neumaticos: { seco: 1.7, lluvioso: 1.0, extremo: 2.8 }
       },
-      imagen: "https://images.footballfanatics.com/scuderia-ferrari/scuderai-ferrari-sf23-no16-monaco-gp-charles-leclerc-1:43-looksmart-model_ss5_p-200370407+pv-1+u-pm6mdiueljifgonqtdlk+v-ovyepy7vdglc7gec2clm.jpg?_hv=2&w=900"
+      conduccion_agresiva: {
+        velocidad_promedio_kmh: 330,
+        consumo_combustible: { seco: 2.7, lluvioso: 3.0, extremo: 3.5 },
+        desgaste_neumaticos: { seco: 2.5, lluvioso: 1.6, extremo: 4.0 }
+      },
+      ahorro_combustible: {
+        velocidad_promedio_kmh: 290,
+        consumo_combustible: { seco: 1.8, lluvioso: 2.0, extremo: 2.4 },
+        desgaste_neumaticos: { seco: 1.2, lluvioso: 0.7, extremo: 2.0 }
+      }
     },
-    {
-      equipo: "McLaren",
-      modelo: "MCL36",
-      motor: "Mercedes",
-      velocidad_maxima_kmh: 345,
-      aceleracion_0_100: 2.8,
-      pilotos: [7, 8],
-      rendimiento: {
-        conduccion_normal: {
-          velocidad_promedio_kmh: 325,
-          consumo_combustible: { seco: 2.0, lluvioso: 2.3, extremo: 2.6 },
-          desgaste_neumaticos: { seco: 1.6, lluvioso: 1.1, extremo: 2.7 }
-        },
-        conduccion_agresiva: {
-          velocidad_promedio_kmh: 340,
-          consumo_combustible: { seco: 2.5, lluvioso: 2.8, extremo: 3.3 },
-          desgaste_neumaticos: { seco: 2.2, lluvioso: 1.4, extremo: 3.9 }
-        },
-        ahorro_combustible: {
-          velocidad_promedio_kmh: 300,
-          consumo_combustible: { seco: 1.7, lluvioso: 1.9, extremo: 2.3 },
-          desgaste_neumaticos: { seco: 1.1, lluvioso: 0.8, extremo: 2.1 }
-        }
+    imagen: "https://images.footballfanatics.com/scuderia-ferrari/scuderai-ferrari-sf23-no16-monaco-gp-charles-leclerc-1:43-looksmart-model_ss5_p-200370407+pv-1+u-pm6mdiueljifgonqtdlk+v-ovyepy7vdglc7gec2clm.jpg?_hv=2&w=900"
+  },
+  {
+    equipo: "McLaren",
+    modelo: "MCL36",
+    motor: "Mercedes",
+    velocidad_maxima_kmh: 345,
+    aceleracion_0_100: 2.8,
+    pilotos: [7, 8],
+    rendimiento: {
+      conduccion_normal: {
+        velocidad_promedio_kmh: 325,
+        consumo_combustible: { seco: 2.0, lluvioso: 2.3, extremo: 2.6 },
+        desgaste_neumaticos: { seco: 1.6, lluvioso: 1.1, extremo: 2.7 }
       },
-      imagen: "https://p.turbosquid.com/ts-thumb/oc/3GrsVt/tJ/mcl60_0000_1200/jpg/1679300174/300x300/sharp_fit_q85/96fce7fc27069cb982363d6ca81746b3b99e4cf2/mcl60_0000_1200.jpg"
-    }
-  ];
+      conduccion_agresiva: {
+        velocidad_promedio_kmh: 340,
+        consumo_combustible: { seco: 2.5, lluvioso: 2.8, extremo: 3.3 },
+        desgaste_neumaticos: { seco: 2.2, lluvioso: 1.4, extremo: 3.9 }
+      },
+      ahorro_combustible: {
+        velocidad_promedio_kmh: 300,
+        consumo_combustible: { seco: 1.7, lluvioso: 1.9, extremo: 2.3 },
+        desgaste_neumaticos: { seco: 1.1, lluvioso: 0.8, extremo: 2.1 }
+      }
+    },
+    imagen: "https://p.turbosquid.com/ts-thumb/oc/3GrsVt/tJ/mcl60_0000_1200/jpg/1679300174/300x300/sharp_fit_q85/96fce7fc27069cb982363d6ca81746b3b99e4cf2/mcl60_0000_1200.jpg"
+  },
+  {
+    equipo: "Williams",
+    modelo: "FW46",
+    motor: "Mercedes",
+    velocidad_maxima_kmh: 340,
+    aceleracion_0_100: 2.9,
+    pilotos: [9, 10],
+    rendimiento: {
+      conduccion_normal: {
+        velocidad_promedio_kmh: 305,
+        consumo_combustible: { seco: 2.2, lluvioso: 2.4, extremo: 2.6 },
+        desgaste_neumaticos: { seco: 1.7, lluvioso: 1.0, extremo: 2.6 }
+      },
+      conduccion_agresiva: {
+        velocidad_promedio_kmh: 325,
+        consumo_combustible: { seco: 2.7, lluvioso: 3.0, extremo: 3.3 },
+        desgaste_neumaticos: { seco: 2.4, lluvioso: 1.5, extremo: 3.8 }
+      },
+      ahorro_combustible: {
+        velocidad_promedio_kmh: 285,
+        consumo_combustible: { seco: 1.8, lluvioso: 2.0, extremo: 2.2 },
+        desgaste_neumaticos: { seco: 1.2, lluvioso: 0.7, extremo: 1.9 }
+      }
+    },
+    imagen: "https://www.renderhub.com/cactus3d/f1-williams-fw46-2024/f1-williams-fw46-2024-01.jpg"
+  },
+  {
+    equipo: "Haas",
+    modelo: "VF-24",
+    motor: "Ferrari",
+    velocidad_maxima_kmh: 335,
+    aceleracion_0_100: 2.9,
+    pilotos: [11, 12],
+    rendimiento: {
+      conduccion_normal: {
+        velocidad_promedio_kmh: 300,
+        consumo_combustible: { seco: 2.3, lluvioso: 2.5, extremo: 2.8 },
+        desgaste_neumaticos: { seco: 1.8, lluvioso: 1.1, extremo: 2.9 }
+      },
+      conduccion_agresiva: {
+        velocidad_promedio_kmh: 320,
+        consumo_combustible: { seco: 2.8, lluvioso: 3.1, extremo: 3.6 },
+        desgaste_neumaticos: { seco: 2.5, lluvioso: 1.7, extremo: 4.0 }
+      },
+      ahorro_combustible: {
+        velocidad_promedio_kmh: 280,
+        consumo_combustible: { seco: 1.9, lluvioso: 2.1, extremo: 2.4 },
+        desgaste_neumaticos: { seco: 1.3, lluvioso: 0.9, extremo: 2.2 }
+      }
+    },
+    imagen: "https://www.renderhub.com/cactus3d/f1-haas-vf-24-2024/f1-haas-vf-24-2024-01.jpg"
+  },
+  {
+    equipo: "Aston Martin",
+    modelo: "AMR24",
+    motor: "Mercedes",
+    velocidad_maxima_kmh: 350,
+    aceleracion_0_100: 2.7,
+    pilotos: [13, 14],
+    rendimiento: {
+      conduccion_normal: {
+        velocidad_promedio_kmh: 310,
+        consumo_combustible: { seco: 2.1, lluvioso: 2.3, extremo: 2.6 },
+        desgaste_neumaticos: { seco: 1.6, lluvioso: 0.9, extremo: 2.6 }
+      },
+      conduccion_agresiva: {
+        velocidad_promedio_kmh: 330,
+        consumo_combustible: { seco: 2.6, lluvioso: 2.9, extremo: 3.3 },
+        desgaste_neumaticos: { seco: 2.3, lluvioso: 1.5, extremo: 3.9 }
+      },
+      ahorro_combustible: {
+        velocidad_promedio_kmh: 290,
+        consumo_combustible: { seco: 1.7, lluvioso: 1.9, extremo: 2.2 },
+        desgaste_neumaticos: { seco: 1.1, lluvioso: 0.6, extremo: 1.8 }
+      }
+    },
+    imagen: "https://p.turbosquid.com/ts-thumb/dV/0kf54j/Y3/amr4_0000/jpg/1709274661/600x600/fit_q87/cd79635aa6515936dc35bce6f277043047a3ecde/amr4_0000.jpg"
+  },
+  {
+    equipo: "Racing Bulls",
+    modelo: "VCARB01",
+    motor: "Honda RBPT",
+    velocidad_maxima_kmh: 340,
+    aceleracion_0_100: 2.8,
+    pilotos: [15, 16],
+    rendimiento: {
+      conduccion_normal: {
+        velocidad_promedio_kmh: 305,
+        consumo_combustible: { seco: 2.2, lluvioso: 2.4, extremo: 2.7 },
+        desgaste_neumaticos: { seco: 1.7, lluvioso: 1.1, extremo: 2.8 }
+      },
+      conduccion_agresiva: {
+        velocidad_promedio_kmh: 325,
+        consumo_combustible: { seco: 2.8, lluvioso: 3.1, extremo: 3.5 },
+        desgaste_neumaticos: { seco: 2.4, lluvioso: 1.6, extremo: 4.0 }
+      },
+      ahorro_combustible: {
+        velocidad_promedio_kmh: 285,
+        consumo_combustible: { seco: 1.9, lluvioso: 2.1, extremo: 2.3 },
+        desgaste_neumaticos: { seco: 1.3, lluvioso: 0.8, extremo: 2.1 }
+      }
+    },
+    imagen: "https://www.raceland.eu/$WS/raceland/websale8_shop-raceland/produkte/medien/bilder/gross/20-39521.jpg"
+  },
+  {
+    equipo: "Alpine",
+    modelo: "A524",
+    motor: "Renault",
+    velocidad_maxima_kmh: 345,
+    aceleracion_0_100: 2.8,
+    pilotos: [17, 18],
+    rendimiento: {
+      conduccion_normal: {
+        velocidad_promedio_kmh: 310,
+        consumo_combustible: { seco: 2.2, lluvioso: 2.5, extremo: 2.8 },
+        desgaste_neumaticos: { seco: 1.6, lluvioso: 1.0, extremo: 2.7 }
+      },
+      conduccion_agresiva: {
+        velocidad_promedio_kmh: 330,
+        consumo_combustible: { seco: 2.7, lluvioso: 3.0, extremo: 3.4 },
+        desgaste_neumaticos: { seco: 2.4, lluvioso: 1.5, extremo: 3.8 }
+      },
+      ahorro_combustible: {
+        velocidad_promedio_kmh: 290,
+        consumo_combustible: { seco: 1.8, lluvioso: 2.0, extremo: 2.3 },
+        desgaste_neumaticos: { seco: 1.2, lluvioso: 0.7, extremo: 2.0 }
+      }
+    },
+    imagen: "https://p.turbosquid.com/ts-thumb/Ya/otN6eu/LE/turbosquid_preview/jpg/1707394178/600x600/fit_q87/850f5b5bdc3126c0881473cde13e23636dc5e957/turbosquid_preview.jpg"
+  }
+];
+
   
   
   document.addEventListener('DOMContentLoaded', () => {
